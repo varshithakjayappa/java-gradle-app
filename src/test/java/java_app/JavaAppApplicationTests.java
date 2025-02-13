ecf96bd2204cefcf768cedd227927c3a6e856c1c
kurimari <kurimari@gmail.com>
2025-02-13 14:46:20 +0100

    initial commit

---
+++ src/test/java/java_app/JavaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package java_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
